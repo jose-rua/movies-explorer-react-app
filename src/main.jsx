3eeb4bf7e057208cdd4bf9e82c3467e5a956fdc3
José Rua <j.rua@runtime-revolution.com>
2026-01-22 15:11:13 +0000

    setup react router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Movies from './Movies/Movies'
+
+import ReactDOM from 'react-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: 'movies', element: <Movies /> },
+     // { path: '*', element: <NotFoundPage /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
